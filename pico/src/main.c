7fc75b821723b2a0caa0ebe7c8b96083ff7a3ea9
Yasushi SHOJI <yashi@spacecubics.com>
2023-07-07 16:47:25 +0900

    pico: Add blinky as the first code
    RPi board pico の最初のコードとして、Lチカを入れておく。今後、関数を追
    加し Lチカ部は消すことになる。
    
    また west.yml にある通り、当面 Zephyr v3.4 を使う。
    
    Commit message の例としても使えるように、本文は日本語で記載した。
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

---
+++ pico/src/main.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+int main(void)
+{
+	const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+	int ret;
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (true) {
+		(void)gpio_pin_toggle_dt(&led);
+		k_sleep(K_SECONDS(1));
+	}
+	return 0;
+}
