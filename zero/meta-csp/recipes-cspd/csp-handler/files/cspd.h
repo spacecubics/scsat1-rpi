f4b9be4738b58586a2ff3f1593a57410bc3c0fcb
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-07-07 11:11:28 +0900

    zero/meta-csp: Reformat source codes
    This commit reformats some source codes.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- zero/meta-csp/recipes-cspd/csp-handler/files/cspd.h
+++ zero/meta-csp/recipes-cspd/csp-handler/files/cspd.h
@@ -1,26 +1,31 @@
-#ifndef CSPD_H
-#define CSPD_H
+/*
+ * Copyright (c) 2024 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <csp/csp.h>
 
-#define PORT_A 10 /* for csp_server_client */
+#define PORT_A (10U) /* for csp_server_client */
 
 #ifndef MAIN_OBC_CAN_ADDR
-#define MAIN_OBC_CAN_ADDR 1
+#define MAIN_OBC_CAN_ADDR (1U)
 #endif /* MAIN_OBC_CAN_ADDR */
 
 #ifndef RPI_ZERO_CAN_ADDR
-#define RPI_ZERO_CAN_ADDR 2
+#define RPI_ZERO_CAN_ADDR (2U)
 #endif /* RPI_ZERO_CAN_ADDR */
 
 #ifndef RPI_ZERO_UART_ADDR
-#define RPI_ZERO_UART_ADDR 3
+#define RPI_ZERO_UART_ADDR (3U)
 #endif /* RPI_ZERO_UART_ADDR */
 
 #ifndef RPI_PICO_UART_ADDR
-#define RPI_PICO_UART_ADDR 4
+#define RPI_PICO_UART_ADDR (4U)
 #endif /* RPI_PICO_UART_ADDR */
 
 /* handler.c */
@@ -28,5 +33,3 @@ void *handle_csp_packet(void *param);
 
 /* router.c */
 int start_csp_router(void);
-
-#endif /* CSPD_H */
