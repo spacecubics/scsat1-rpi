921416ba4a323bbb5b268f9dc915f81b40e7b293
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-17 14:48:43 +0900

    zero: csp-handler: Add File Upload (OPEN) command
    This commit adds the open file command needed for file upload from
    the Ground System to SC-Sat1. This command includes the session ID
    and the target file name. On the SC-Sat1 side, it associates the
    session ID with the file name and opens the target file with write
    permissions. If the target file does not exist, it will be newly
    created. The session ID will be included in subsequent Data Packets
    (DATA).
    
    For uploads, the file that can be opened is defined by
    UPLOAD_MAX_SESSION.
    
    This implementation also uses `g_slist` for session management;
    however, since upload commands are processed sequentially from the
    CSP server thread, mutual exclusion is not implemented at this time.
    
    In a subsequent commit, the DATA and CLOSE commands will also be
    added.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- zero/meta-csp/recipes-cspd/csp-handler/files/file.c
+++ zero/meta-csp/recipes-cspd/csp-handler/files/file.c
@@ -10,12 +10,61 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <systemd/sd-journal.h>
+#include <sys/stat.h>
+#include "cspd.h"
 #include "utils.h"
+#include "upload.h"
 
 #define FILE_WORK_THREAD_SLEEP_USEC (10000U)
+#define UPLOAD_MAX_SESSION          (5U)
+
+/* Command size */
+#define FILE_CMD_MIN_SIZE (1U)
+
+/* Command ID */
+#define FILE_UPLOAD_OPEN_CMD (2U)
 
 static GQueue file_work_queue;
 static pthread_t file_work;
+
+static void csp_file_work(csp_packet_t *packet)
+{
+	int ret = 0;
+	uint8_t command_id;
+
+	if (packet == NULL) {
+		ret = -EINVAL;
+		command_id = CSP_UNKNOWN_COMMAND_ID;
+		goto end;
+	}
+
+	if (packet->length < FILE_CMD_MIN_SIZE) {
+		sd_journal_print(LOG_ERR, "Invalide command size: %d", packet->length);
+		ret = -EINVAL;
+		command_id = CSP_UNKNOWN_COMMAND_ID;
+		goto free;
+	}
+
+	command_id = packet->data[CSP_COMMAND_ID_OFFSET];
+
+	switch (command_id) {
+	case FILE_UPLOAD_OPEN_CMD:
+		file_upload_open_cmd(command_id, packet);
+		break;
+	default:
+		sd_journal_print(LOG_ERR, "Unkown command code: %d", command_id);
+		ret = -EINVAL;
+		command_id = CSP_UNKNOWN_COMMAND_ID;
+		break;
+	}
+
+free:
+	if (ret < 0) {
+	}
+
+end:
+	return;
+}
 
 static void *file_work_thread(void *arg)
 {
@@ -29,7 +78,7 @@ static void *file_work_thread(void *arg)
 			usleep(FILE_WORK_THREAD_SLEEP_USEC);
 			continue;
 		}
-		csp_buffer_free(packet);
+		csp_file_work(packet);
 	}
 
 	pthread_exit(NULL);
@@ -52,4 +101,6 @@ void file_handler_init(void)
 	} else {
 		sd_journal_print(LOG_INFO, "Start the file work thread");
 	}
+
+	upload_handler_init();
 }
