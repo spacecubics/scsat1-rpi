95712849ef24bd3b97681b96ab15d4263c2b0ce6
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-22 14:46:50 +0900

    zero: csp-handler: Fix missing error telemetry
    Fixes an issue where reply telemetry was not returned for
    file-related commands with invalid sizes or command IDs.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- zero/meta-csp/recipes-cspd/csp-handler/files/file.h
+++ zero/meta-csp/recipes-cspd/csp-handler/files/file.h
@@ -8,5 +8,10 @@
 
 #include <csp/csp.h>
 
+struct file_err_reply_telemetry {
+	uint8_t telemetry_id;
+	uint32_t error_code;
+} __attribute__((__packed__));
+
 void file_handler_init(void);
 void file_handler(csp_packet_t *packet);
